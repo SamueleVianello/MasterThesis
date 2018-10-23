49a08ad8a5f6d8b69398d9809e8b8e0f11a8140d
SamueleVianello <samuele.vianello@gmail.com>
2018-10-23 18:18:45 +0200

    Improved test on variance and added results to excel file.

--- R_code/Bates/test_variance_process_estimation.R
+++ R_code/Bates/test_variance_process_estimation.R
@@ -4,8 +4,18 @@ rolling_stdev = function(data, window, skip=1){
   res= sd(data[1:window])
   
   idx=seq(1,length(data), skip)
-  for(i in 2:length(idx)){
+  for(i in 2:(length(idx)-1)){
     res= c(res, sd(data[idx[i]:(idx[i]+window)]))
+  }
+  return(res)
+}
+
+rolling_variance = function(data, window, skip=1){
+  res= var(data[1:window])
+  
+  idx=seq(1,length(data), skip)
+  for(i in 2:(length(idx)-1)){
+    res= c(res, var(data[idx[i]:(idx[i]+window)]))
   }
   return(res)
 }
@@ -18,7 +28,7 @@ dn =255*6
 asset = eurostoxx
 
 # DAILY SPACED *** DAILY SPACED *** DAILY SPACED *** DAILY SPACED ***
-roll_sd = rolling_stdev(rev(asset)[1:dn],window=5,skip = 1)
+roll_sd = rolling_stdev(rev(asset)[1:dn],window=5,skip = 1)*sqrt(255)
 
 plot(roll_sd, type='l', lwd=2)
 #test = acf(roll_sd, lag.max = 1)
@@ -45,7 +55,7 @@ c(k=k,eta=eta,theta=theta)
 
 
 # WEEKLY SPACED *** WEEKLY SPACED *** WEEKLY SPACED *** WEEKLY SPACED ***
-roll_sd = rolling_stdev(rev(asset)[1:dn],window=5,skip = 5)
+roll_sd = rolling_stdev(rev(asset)[1:dn],window=5,skip = 5)*sqrt(255)
 
 lines((1:length(roll_sd))*5-4,roll_sd, col='blue',lwd=2)
 #test = acf(roll_sd, lag.max = 1)
@@ -70,7 +80,7 @@ c(k=k,eta=eta,theta=theta)
 
 
 # MONTHLY SPACED *** MONTHLY SPACED *** MONTHLY SPACED *** MONTHLY SPACED *** 
-roll_sd = rolling_stdev(rev(asset)[1:dn],window=21,skip = 21)
+roll_sd = rolling_stdev(rev(asset)[1:dn],window=21,skip = 21)*sqrt(255)
 
 plot((1:length(roll_sd))*5-4,roll_sd,type='l', col='blue',lwd=2)
 #test = acf(roll_sd, lag.max = 1)
@@ -90,4 +100,34 @@ eta = delta**2 / k
 theta = 2*delta
 
 c(k=k,eta=eta,theta=theta)
+
+
+
+
+
+# VASICEK 
+roll_var = rolling_variance(rev(asset)[1:dn],window=5,skip = 5)*255
+
+
+plot((1:length(roll_var))*5-4,roll_var,type='l', col='blue',lwd=2)
+#test = acf(roll_sd, lag.max = 1)
+
+coef_lm = lm(roll_var[-1]~roll_var[-length(roll_var)])
+# summary(coef_lm)
+
+b0 = coef_lm$coefficients[1]
+b1 = coef_lm$coefficients[2]
+
+dt= 5/255
+
+
+k = (1-b1)/dt
+eta = b0/k
+theta = sqrt(var(coef_lm$residuals)/dt)
+
+
+c(k=k,eta=eta,theta=theta/(sd(asset)*sqrt(255)))
+
+
+
 
